b476b29b2d60726d4ddbda90739fbe239971dba8
wielki.borsuk <wielki.borsuk@gmail.com>
2020-09-19 15:29:40 +0200

    allow querying for forecasts by date

--- src/main/java/com/sda/javapol12/weather/Main.java
+++ src/main/java/com/sda/javapol12/weather/Main.java
@@ -5,11 +5,16 @@ import com.sda.javapol12.weather.forecastsource.openweather.OpenWeather;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         String key = Files.readAllLines(Paths.get("key.txt")).get(0).trim();
         OpenWeather forecastSource = new OpenWeather(key);
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+
+        System.out.println(forecastSource.getForecast(50.041187, 21.999121, tomorrow.plusDays(5)));
+        System.out.println(forecastSource.getForecast("Rzeszow", tomorrow.plusDays(5)));
 
         System.out.println(forecastSource.getForecast(50.041187, 21.999121));
         System.out.println(forecastSource.getForecast("Rzeszow"));
--- src/main/java/com/sda/javapol12/weather/forecastsource/openweather/OpenWeather.java
+++ src/main/java/com/sda/javapol12/weather/forecastsource/openweather/OpenWeather.java
@@ -1,14 +1,15 @@
 package com.sda.javapol12.weather.forecastsource.openweather;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sda.javapol12.weather.forecastsource.openweather.model.Coords;
-import com.sda.javapol12.weather.forecastsource.openweather.model.OpenWeatherCurrentForecast;
-import com.sda.javapol12.weather.forecastsource.openweather.model.OpenWeatherResponse;
-import com.sda.javapol12.weather.forecastsource.openweather.model.OpenWeatherWeatherResponse;
+import com.sda.javapol12.weather.forecastsource.openweather.model.*;
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.apache.http.client.fluent.Request;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.List;
 
 public class OpenWeather {
     private final static String URI_PATTERN = "https://api.openweathermap.org/data/2.5/onecall?lat=%f&lon=%f&exclude=minutely,hourly&units=metric&appid=%s";
@@ -21,30 +22,50 @@ public class OpenWeather {
         this.key = key;
     }
 
-    public OpenWeatherCurrentForecast getForecast(String city) {
+    public OpenWeatherDailyForecast getForecast(String city) {
+        return getForecast(city, getTomorrow());
+    }
+
+    public OpenWeatherDailyForecast getForecast(String city, LocalDate date) {
         try {
             String uri = String.format(CITY_PATTERN, city, key);
             String response = Request.Get(uri)
                     .execute().returnContent().asString();
             Coords coords = MAPPER.readValue(response, OpenWeatherWeatherResponse.class).getCoord();
-            return getForecast(coords.getLat(), coords.getLon());
+            return getForecast(coords.getLat(), coords.getLon(), date);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    public OpenWeatherCurrentForecast getForecast(double lat, double lon) {
+    public OpenWeatherDailyForecast getForecast(double lat, double lon) {
+        return getForecast(lat, lon, getTomorrow());
+    }
+
+    public OpenWeatherDailyForecast getForecast(double lat, double lon, LocalDate date) {
         try {
             String uri = String.format(URI_PATTERN, lat, lon, key);
 
             String response = Request.Get(uri)
                     .execute().returnContent().asString();
             OpenWeatherResponse openWeatherResponse = MAPPER.readValue(response, OpenWeatherResponse.class);
-            return openWeatherResponse.getCurrent();
+            return findForecastForDate(openWeatherResponse.getDaily(), date);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    private OpenWeatherDailyForecast findForecastForDate(List<OpenWeatherDailyForecast> dailyForecasts, LocalDate date) {
+        return dailyForecasts.stream()
+                .filter(forecast -> date.equals(Instant.ofEpochSecond(forecast.getDateTime())
+                        .atZone(ZoneId.systemDefault()).toLocalDate()))
+                .findAny()
+                .orElse(null);
+    }
+
+    private LocalDate getTomorrow() {
+        return LocalDate.now().plusDays(1);
     }
 }
